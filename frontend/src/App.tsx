f4eadfe4d75726c30a8de746e215aa3f70a42af3
Shahed_ali <shahed@beyondscale.tech>
2023-04-14 23:24:11 +0530

    Created dark theme for the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React, { FC, ReactElement } from 'react';
-
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
 // FC stands for React Functional Component which is an interface which includes all the params
 // that can be passed to this component.
 
 const App: FC = (): ReactElement => {
   return (
-    <div className="App">
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
       <h1>Hello world</h1>
-    </div>
+    </ThemeProvider>
   );
 };
 
